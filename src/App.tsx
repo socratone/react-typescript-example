fe51d48514c9f6ef077f6081ba30486651714fde
John Kim <john@gymtinc.com>
2021-10-23 11:44:48 +0900

    feat: 타입 상속 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import TypeInheritance from './TypeInheritance';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TypeInheritance
+        title="타입스크립트"
+        description="타입스크립트는 재미있다."
+      />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  padding: 20px;
+`;
 
 export default App;
---
+++ src/TypeInheritance/index.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+type ParentProps = {
+  title: string;
+};
+
+type ChildProps = {
+  description: string;
+};
+
+const TypeInheritance = ({ title, description }: ParentProps & ChildProps) => {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid gainsboro;
+  border-radius: 10px;
+`;
+
+const Title = styled.h2``;
+
+const Description = styled.p``;
+
+export default TypeInheritance;
